a384f1af5e09bc2123a4f0cb9f0a34919841984e
awirawan <adwirawp@gmail.com>
2022-04-02 13:53:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androiddevcodingtest/Recipe.kt
@@ -0,0 +1,16 @@
+package com.example.androiddevcodingtest
+
+data class Recipe(
+    val id: String,
+    val calories: String,
+    val carbos: String,
+    val description: String,
+    val difficulty: Int,
+    val fats: String,
+    val headlines: String,
+    val image: String,
+    val name: String,
+    val protein: String,
+    val thumb: String,
+    val time: String
+)
